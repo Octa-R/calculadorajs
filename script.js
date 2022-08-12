8192f8420cb5c98f3cb7c140a77856435085a173
Octa <ruarteoctavio8@gmail.com>
2022-08-12 18:02:42 -0300

    cambios de estilo y responsive

--- script.js
+++ script.js
@@ -1,24 +1,3 @@
-document.querySelectorAll("[data-number]").forEach((e) => {
-  e.addEventListener("click", () => {
-    calculator.appendNumber(e.innerText);
-  });
-});
-
-document.querySelector("[data-delete").addEventListener("click", (e) => {
-  calculator.delete();
-});
-document.querySelector("[data-equals]").addEventListener("click", (e) => {
-  calculator.compute();
-});
-document
-  .querySelector("[data-all-clear]")
-  .addEventListener("click", function () {
-    calculator.clear();
-  });
-document.querySelector("[data-sqrt]").addEventListener("click", () => {
-  calculator.sqrt();
-});
-
 class Calculator {
   previousNumber = "";
   currentNumber = "";
@@ -26,30 +5,45 @@ class Calculator {
   currentDisplay;
   operation = undefined;
   result = undefined;
+  operationChar = "";
+
   constructor(curr, prev) {
     this.previousDisplay = prev;
     this.currentDisplay = curr;
   }
+
+  map = {
+    "+": (a, b) => a + b,
+    "-": (a, b) => a - b,
+    "/": (a, b) => a / b,
+    "*": (a, b) => a * b,
+    "^": (a, b) => Math.pow(a, b)
+  };
+
   clear() {
     this.previousNumber = "";
     this.currentNumber = "";
     this.operation = undefined;
     this.updateDisplay();
   }
+
   delete() {
     this.currentNumber = this.currentNumber.toString().slice(0, -1);
     this.updateDisplay();
   }
+
   updateDisplay() {
     this.currentDisplay.innerText = this.getDisplayNumber(this.currentNumber);
+
     if (this.operation != null) {
       this.previousDisplay.innerText = `${this.getDisplayNumber(
         this.previousNumber
-      )} ${this.operation.char}`;
+      )} ${this.operationChar}`;
     } else {
       this.previousDisplay.innerText = "";
     }
   }
+
   getDisplayNumber(number) {
     const strNum = number.toString();
     const intDigits = parseFloat(strNum.split(".")[0]);
@@ -68,29 +62,34 @@ class Calculator {
       return intDisplay;
     }
   }
+
   appendNumber(number) {
     this.currentNumber += number.toString();
     this.updateDisplay();
   }
-  setOperation(operation) {
+
+  setOperation(operationChar) {
     if (this.currentNumber === "") return;
     if (this.previousNumber !== "") {
       this.compute();
     }
-    this.operation = operation;
+    this.operation = this.map[operationChar];
+    this.operationChar = operationChar;
     this.previousNumber = this.currentNumber;
     this.currentNumber = "";
     this.updateDisplay();
   }
+
   compute() {
     const prev = parseFloat(this.previousNumber);
     let curr = parseFloat(this.currentNumber);
     if (isNaN(prev) || isNaN(curr)) return;
-    this.currentNumber = this.operation.operation(prev, curr);
+    this.currentNumber = this.operation(prev, curr);
     this.operation = undefined;
     this.previousNumber = "";
     this.updateDisplay();
   }
+
   sqrt() {
     if (this.currentNumber === "") return;
     this.currentNumber = Math.sqrt(parseFloat(this.currentNumber));
@@ -98,31 +97,47 @@ class Calculator {
   }
 }
 
-const sub = {
-  char: "-",
-  operation: (a, b) => a - b,
-};
-const add = {
-  char: "+",
-  operation: (a, b) => a + b,
-};
-const divide = {
-  char: "÷",
-  operation: (a, b) => a / b,
-};
-const multiply = {
-  char: "*",
-  operation: (a, b) => a * b,
-};
-const pow = {
-  char: "^",
-  operation: (a, b) => Math.pow(a, b),
-};
 
-const operationClick = (op) => {
-  calculator.setOperation(op);
-};
+function main() {
+  const numberButtons = document.querySelectorAll("[data-number]");
+  const operationButtons = document.querySelectorAll("[data-operation]");
+  const sqrtButton = document.querySelector("[data-sqrt]");
+  const equalsButton = document.querySelector("[data-equals]");
+  const deleteButton = document.querySelector("[data-delete]");
+  const allClearButton = document.querySelector("[data-all-clear]");
 
-const previousDisplay = document.querySelector("[data-previous-operand]");
-const currentDisplay = document.querySelector("[data-current-operand]");
-const calculator = new Calculator(currentDisplay, previousDisplay);
+  numberButtons.forEach((e) => {
+    e.addEventListener("click", () => {
+      calculator.appendNumber(e.innerText);
+    });
+  });
+
+  operationButtons.forEach(btn => {
+    btn.addEventListener("click", (e) => {
+      const operationChar = e.target.innerText;
+      calculator.setOperation(operationChar);
+    });
+  });
+
+  deleteButton.addEventListener("click", (e) => {
+    calculator.delete();
+  });
+
+  equalsButton.addEventListener("click", (e) => {
+    calculator.compute();
+  });
+
+  allClearButton.addEventListener("click", function () {
+    calculator.clear();
+  });
+
+  sqrtButton.addEventListener("click", () => {
+    calculator.sqrt();
+  });
+
+  const previousDisplay = document.querySelector("[data-previous-operand]");
+  const currentDisplay = document.querySelector("[data-current-operand]");
+  const calculator = new Calculator(currentDisplay, previousDisplay);
+}
+
+main();
